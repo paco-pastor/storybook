7e821f43caae70f68b22294c8960b915b4d7ee3d
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-08-08 22:33:43 +0200

    🎉 Initial commit

---
+++ src/stories/headers/Title.stories.js
@@ -0,0 +1,13 @@
+import Title from './Title.svelte';
+
+export default {
+  title: 'Headers/Title',
+  component: Title,
+};
+
+export const Classic = {
+  args: {
+    classic: true,
+    label: 'Title',
+  },
+};
